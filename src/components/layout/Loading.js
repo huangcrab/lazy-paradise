af3bb09e1d62922268d140a228f028502f9ea487
sean <huangcrab@gmail.com>
2018-11-07 14:04:37 -0500

    loading screen to videos

--- src/components/layout/Loading.js
+++ src/components/layout/Loading.js
@@ -1,13 +1,41 @@
 import React, { Component } from "react";
+import styled, { keyframes } from "styled-components";
+
+const flash = keyframes`
+  
+  50% {opacity:0; }
+  `;
+
+const Loader = styled.div`
+  height: 100vh;
+  width: 100vw;
+  background: #000;
+  display: flex;
+  z-index: 1000;
+  position: relative;
+`;
+const Item = styled.div`
+  background: #fff;
+  position: relative;
+  height: 80px;
+  width: 80px;
+  border-radius: 20px;
+  color: #000;
+  font-size: 80px;
+  padding-bottom: 5px;
+  text-align: center;
+  font-weight: bold;
+  margin: auto;
+  font-family: "Segoe UI", Tahoma, Geneva, Verdana, sans-serif;
+  animation: ${flash} 0.5s ease infinite;
+`;
 
 class Loading extends Component {
   render() {
     return (
-      <div className="loading">
-        <div className="animated infinite flash" id="box">
-          X
-        </div>
-      </div>
+      <Loader>
+        <Item>X</Item>
+      </Loader>
     );
   }
 }
--- src/components/layout/VideoBackGound.js
+++ src/components/layout/VideoBackGound.js
@@ -1,12 +1,30 @@
 import React, { Component } from "react";
+import Loading from "./Loading";
 
 export default class VideoBackGound extends Component {
+  state = {
+    loaded: false
+  };
+  constructor() {
+    super();
+    this.videoRef = React.createRef();
+  }
+
+  componentDidMount() {
+    this.videoRef.addEventListener("loadedmetadata", () => {
+      console.log(this.videoRef.readyState);
+      this.setState({
+        loaded: true
+      });
+    });
+  }
   render() {
     const { video } = this.props;
     return (
       <div className="video-overlay">
+        {this.state.loaded ? null : <Loading />}
         <video
-          ref="videoRef"
+          ref={elem => (this.videoRef = elem)}
           className="fullscreen-video"
           src={video}
           autoPlay
@@ -14,6 +32,7 @@ export default class VideoBackGound extends Component {
           type="video/mp4"
           muted
         />
+        {}
       </div>
     );
   }
